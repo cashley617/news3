3e919addd777addfced92b5582b307f7040c5722
Cate Ashley <catemarie.ashley@gmail.com>
2019-09-27 19:08:40 -0600

    removed comments from schema

--- models/Article.js
+++ models/Article.js
@@ -9,17 +9,6 @@ let ArticleSchema = new Schema({
         type: String,
         required: true
     },
-
-    // link: { 
-    //     type: String,
-    //     required: true
-    // },
-
-    // image: {
-    //     type: String,
-    //     required: true
-    // },
-
     note: {
         type: Schema.Types.ObjectId,
         ref: "Note"
